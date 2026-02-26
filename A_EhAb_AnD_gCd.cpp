13f6da8fafe7324afd12aba7cad58f5710a3a6db
kawser0x <kawserswe@gmail.com>
2026-02-26 19:13:27 +0600

    https://codeforces.com/problemset/problem/1325/A

---
+++ A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t ;
+    cin >> t ;
+    while(t--){
+        int s ;
+        cin >> s ;
+        cout << 1 <<' ' << s-1 << '\n' ;
+    }
+    return 0 ;
+}
